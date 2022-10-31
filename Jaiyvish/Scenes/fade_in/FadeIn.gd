c9d5f1eb99d1823f38abc4bd82b291114fa5468d
Jaiy bozzo <jaipra99@kingshigh.school.nz>
2022-11-01 09:55:08 +1300

    Title screen restart,
    It didn't work so i deleted it all and I'm redoing my code

--- Jaiyvish/Scenes/fade_in/FadeIn.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready():
-	pass
-	
-func _on_MenuButton_pressed():
-	get_tree().change_scene("res://Scenes/World.tcsn")
--- Jaiyvish/Scenes/game/return_to_title.gd
+++
@@ -1,3 +0,0 @@
-extends Button
-func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/fade_in/FadeIn.tscn")
